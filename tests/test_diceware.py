40c60e1d1c76fa78926840b020e06ed4ee082c82
ulif <uli@gnufix.de>
2015-03-25 11:58:34 +0100

    Also restore sys.stdin in argv-handler.

--- tests/test_diceware.py
+++ tests/test_diceware.py
@@ -21,12 +21,14 @@ class FakeRandom(object):
 
 @pytest.fixture(scope="function")
 def argv_handler(request):
-    """This fixture restores sys.argv after tests.
+    """This fixture restores sys.argv and sys.stdin after tests.
     """
     _argv_stored = sys.argv
+    _stdin_stored = sys.stdin
 
     def teardown():
         sys.argv = _argv_stored
+        sys.stdin = _stdin_stored
     request.addfinalizer(teardown)
 
 
