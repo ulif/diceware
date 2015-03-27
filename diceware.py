20723ccaffc4a4dabb69d33a6caba5103e9be476
ulif <uli@gnufix.de>
2015-03-27 11:38:55 +0100

    Add func for version infos.

--- diceware.py
+++ diceware.py
@@ -18,6 +18,36 @@ RE_LANG_CODE = re.compile('^[a-zA-Z]{2}$')
 SPECIAL_CHARS = "~!#$%^&*()-=+[]\{}:;\"'<>?/0123456789"
 
 
+GPL_TEXT = (
+    """
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    """
+    )
+
+
+def print_version():
+    """Output current version and other infos.
+    """
+    print("diceware %s" % __version__)
+    print("Copyright (C) 2015 Uli Fouquet")
+    print("diceware is based on suggestions of Arnold G. Reinhold.")
+    print("See http://diceware.com for details.")
+
+    print("'Diceware' is a trademark of Arnold G. Reinhold.")
+    print(GPL_TEXT)
+
+
 def handle_options(args):
     """Handle commandline options.
     """
