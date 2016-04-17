60ec799fedb8e1956c52f3fc78030e1053098f9a
ulif <uli@gnufix.de>
2016-04-17 14:26:55 +0200

    Update copyright year.

--- diceware/__init__.py
+++ diceware/__init__.py
@@ -1,5 +1,5 @@
 #  diceware -- passphrases to remember
-#  Copyright (C) 2015  Uli Fouquet
+#  Copyright (C) 2015, 2016  Uli Fouquet
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
