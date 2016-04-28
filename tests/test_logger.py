c622fa12f449af6043e7bc23185fdfc052094177
ulif <uli@gnufix.de>
2016-04-28 09:35:04 +0200

    Add test: get logger by name.

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,3 +1,4 @@
+import logging
 from diceware.logger import logger
 
 
@@ -9,3 +10,9 @@ def test_logger_exists():
 def test_logger_has_handler():
     # the logger has at least one handler
     assert len(logger.handlers) > 0
+
+
+def test_get_logger_by_name():
+    # we can get a logger directly from std lib
+    my_logger = logging.getLogger("ulif.diceware")
+    assert len(my_logger.handlers) > 0
