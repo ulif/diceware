a9659b9d97dfdf94ed21ccdc56b24f2f42ae3838
ulif <uli@gnufix.de>
2016-05-06 21:14:31 +0200

    All collected changes give a minor release.
    Especially changing the .ini-file parsing is more than a simple
    bugfix.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ docs_require = [
 
 setup(
     name="diceware",
-    version="0.7.2.dev0",
+    version="0.8.dev0",
     author="Uli Fouquet",
     author_email="uli@gnufix.de",
     description=(
