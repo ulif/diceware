4c29b83fa213804f886bfa44b770703e66f97c53
Hartmut Goebel <h.goebel@crazy-compilers.com>
2019-04-29 10:45:29 +0200

    Eliminate use of `pkg_resources` module.
    Also eliminate the plug-in entry-points in setup.py.
    
    As a negative side-effect, this also eliminated the
    mechanism to plug-in random other sources by installing
    independent packages.

--- setup.py
+++ setup.py
@@ -79,10 +79,5 @@ setup(
         'console_scripts': [
             'diceware = diceware:main',
         ],
-        'diceware_random_sources': [
-            'system = diceware.random_sources:SystemRandomSource',
-            'realdice = diceware.random_sources:RealDiceRandomSource',
-            # add more sources of randomness here...
-        ],
     },
 )
