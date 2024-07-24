8af5b208599e731cb6386614c2446a296a241de4
ulif <uli@gnufix.de>
2024-07-24 15:56:01 +0200

    Upgrade timestamps.

--- diceware/config.py
+++ diceware/config.py
@@ -1,5 +1,5 @@
 #  diceware -- passphrases to remember
-#  Copyright (C) 2015-2017  Uli Fouquet
+#  Copyright (C) 2015-2024  Uli Fouquet
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
