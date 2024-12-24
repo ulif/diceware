f3e959a386b7b234062b05a1181c764df4d66b82
ulif <uli@gnufix.de>
2024-12-24 02:09:49 +0100

    Back to work: v1.0.2.dev0

--- diceware/__about__.py
+++ diceware/__about__.py
@@ -1,4 +1,4 @@
-version = "1.0.1"
+version = "1.0.2.dev0"
 random_sources = {
     'system': 'diceware.random_sources:SystemRandomSource',
     'realdice': 'diceware.random_sources:RealDiceRandomSource',
